bea73d1b4bd4853461f934b5c03e47c0d185f876
Youcef Ben Cheikh <61650100+Youcef-x64@users.noreply.github.com>
2024-04-02 21:50:26 -0500

    Add the logic

--- lab1b/src/main/java/org/example/Main.java
+++ lab1b/src/main/java/org/example/Main.java
@@ -1,7 +1,89 @@
 package org.example;
 
+import org.example.model.Employee;
+import org.example.model.PensionPlan;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Employee> employees = new ArrayList<>();
+
+        employees.add(new Employee(1, "Daniel", "Agar", LocalDate.of(2018, 1, 17),
+                105945.5, new PensionPlan("EX1089", LocalDate.of(2023, 1, 17), 100)));
+        employees.add(new Employee(2, "Benard", "Shaw", LocalDate.of(2019, 4, 3),
+                197750, null));
+        employees.add(new Employee(3, "Carly", "Agar", LocalDate.of(2014, 5, 16),
+                842000.75, new PensionPlan("SM2307", LocalDate.of(2019, 11, 4), 1555.5)));
+        employees.add(new Employee(4, "Wesley", "Schneider", LocalDate.of(2019, 5, 2),
+                74500, null));
+
+        // Task B-1
+        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee e1, Employee e2) {
+                int lastNameComparison = e1.getLastName().compareTo(e2.getLastName());
+                if (lastNameComparison != 0) {
+                    return lastNameComparison;
+                }
+                return Double.compare(e1.getSalary(), e2.getSalary());
+            }
+        });
+
+        System.out.println("Printed in JSON Format");
+        System.out.println(convertEmployeeListToJson(employees));
+
+        // Task B-2
+        List<Employee> filteredEmployees = new ArrayList<>();
+        LocalDate currentDate = LocalDate.now();
+        LocalDate lastDayOfNextMonth = LocalDate.of(currentDate.getYear(),
+                currentDate.getMonth().plus(1), 1).plusMonths(1).minusDays(1);
+        for (Employee employee : employees) {
+            LocalDate fiveYearsAfterStartDate = employee.getEmploymentDate().plusYears(5);
+            if (employee.getPensionPlan() == null && fiveYearsAfterStartDate.isBefore(lastDayOfNextMonth)) {
+                filteredEmployees.add(employee);
+            }
+        }
+
+        System.out.println("Printed in JSON Format");
+        System.out.println(convertEmployeeListToJson(filteredEmployees));
     }
+
+    private static String convertEmployeeListToJson(List<Employee> employeeList) {
+        StringBuilder jsonBuilder = new StringBuilder();
+        jsonBuilder.append("[\n");
+
+        for (int i = 0; i < employeeList.size(); i++) {
+            Employee employee = employeeList.get(i);
+            jsonBuilder.append("{");
+
+            // Add product attributes to JSON object
+            jsonBuilder.append("\"id\": ").append(employee.getId()).append(", ");
+            jsonBuilder.append("\"firstName\": \"").append(employee.getFirstName()).append("\", ");
+            jsonBuilder.append("\"lastName\": \"").append(employee.getLastName()).append("\", ");
+            jsonBuilder.append("\"employmentDate\": ").append(employee.getEmploymentDate()).append(", ");
+            jsonBuilder.append("\"yearlySalary\": ").append(employee.getYearlySalary());
+
+            if (employee.getPensionPlan() != null) {
+                jsonBuilder.append("\"pensionPlan\": {\"planReferenceNumber\": ")
+                        .append(employee.getPensionPlan().getPlanReferenceNumber()).append(", ");
+                jsonBuilder.append("\"enrollmentDate\": ")
+                        .append(employee.getPensionPlan().getEnrollmentDate()).append(", ");
+                jsonBuilder.append("\"monthlyContribution\": ")
+                        .append(employee.getPensionPlan().getMonthlyContribution()).append(", ");
+
+                jsonBuilder.append("}");
+            }
+
+            jsonBuilder.append("}");
+
+            // Add comma if not the last element
+            if (i < employeeList.size() - 1) {
+                jsonBuilder.append(", \n");
+            }
+        }
+
+        jsonBuilder.append("\n]");
+
+        return jsonBuilder.toString();
+    }
+
 }
---
+++ lab1b/src/main/java/org/example/model/Employee.java
@@ -0,0 +1,68 @@
+package org.example.model;
+
+public class Employee {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate employmentDate;
+    private double yearlySalary;
+    private PensionPlan pensionPlan;
+
+    public Employee(long id, String firstName, String lastName, LocalDate employmentDate, double yearlySalary, PensionPlan pensionPlan) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employmentDate = employmentDate;
+        this.yearlySalary = yearlySalary;
+        this.pensionPlan = pensionPlan;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getEmploymentDate() {
+        return employmentDate;
+    }
+
+    public void setEmploymentDate(LocalDate employmentDate) {
+        this.employmentDate = employmentDate;
+    }
+
+    public double getYearlySalary() {
+        return yearlySalary;
+    }
+
+    public void setYearlySalary(double yearlySalary) {
+        this.yearlySalary = yearlySalary;
+    }
+
+    public PensionPlan getPensionPlan() {
+        return pensionPlan;
+    }
+
+    public void setPensionPlan(PensionPlan pensionPlan) {
+        this.pensionPlan = pensionPlan;
+    }
+}
